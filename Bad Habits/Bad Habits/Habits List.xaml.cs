558f7a7a365e4c33dcd6853fc24aef0878e7d2f2
Kirill <blinovk955@gmail.com>
2021-08-01 19:19:04 +0300

    Инициализация проекта

---
+++ Bad Habits/Bad Habits/Habits List.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bad_Habits
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Habits_List : ContentPage
+	{
+		public Habits_List()
+		{
+			InitializeComponent();
+		}
+
+		private void selectHabitsBtn_Clicked(object sender, EventArgs e)
+		{
+			pir1.Focus();
+		}
+
+		private void pir1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			ent1.Text = pir1.SelectedItem.ToString();
+		}
+
+		private async void nextHabitsBtn_Clicked(object sender, EventArgs e)
+		{
+			Label label = new Label() 
+			{ Text = ent1.Text };
+
+			NavigationPage navPage = (NavigationPage)Application.Current.MainPage;
+			IReadOnlyList<Page> navStack = navPage.Navigation.NavigationStack;
+			MainPage homePage = navStack[0] as MainPage;
+
+			homePage.AddHabits(label);
+
+			await Navigation.PopAsync();
+		}
+	}
+}
---
+++ Bad Habits/Bad Habits/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bad_Habits
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void addBtn_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new Habits_List());
+		}
+
+		public void AddHabits(Label label)
+		{
+			MainStackList.Children.Add(label);
+		}
+	}
+}
